ad7c18545ecfbc88a71abcb2a1f3aadda064b3e6
Aman Kumar Yadav <yadaviactaman2030@gmail.com>
2026-01-31 01:00:13 +0530

    Done

---
+++ fact.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# calling the function
+num = int(input("ENter the your number = "));
+print("Factorial of", num, "is:", factorial(num))
